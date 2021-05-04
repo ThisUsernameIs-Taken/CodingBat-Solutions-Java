7d3bbbdea31c81baa23b3218bd7ea515ae12b382
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-05-04 15:40:51 -0400

    Create startOz.java

---
+++ Warmup-1/startOz.java
@@ -0,0 +1,10 @@
+public String startOz(String str) {
+  String oz = "";
+  if (str.length() > 0 && str.charAt(0) == 'o' ){
+    oz += "o";
+  } 
+  if (str.length() > 1 && str.charAt(1) == 'z' ) {
+    oz += "z";
+  }
+  return oz;
+}
