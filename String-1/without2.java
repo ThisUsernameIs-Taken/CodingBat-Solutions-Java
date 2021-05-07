7b9881db17125b86a28aeae449b83440b8dcbf79
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-05-06 21:50:33 -0400

    Create without2.java

---
+++ String-1/without2.java
@@ -0,0 +1,14 @@
+public String without2(String str) {
+  if (str.length() < 2) {
+    return str;
+  } else {
+    if (str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+      if (str.length() == 2) {
+        return "";
+      } else {
+        return str.substring(2);
+      }
+    }
+  }
+  return str;
+}
