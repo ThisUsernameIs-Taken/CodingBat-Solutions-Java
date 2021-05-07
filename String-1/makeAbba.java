3c8e0104bb61883d568605a02b6068ab900ef8da
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-05-06 21:22:22 -0400

    Create makeAbba.java

---
+++ String-1/makeAbba.java
@@ -0,0 +1,5 @@
+/* ABBA are a Swedish pop group formed in Stockholm in 1972 by Agnetha Fältskog, Björn Ulvaeus, Benny Andersson, and Anni-Frid Lyngstad. 
+The group's name is an acronym of the first letters of their first names.*/
+public String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
